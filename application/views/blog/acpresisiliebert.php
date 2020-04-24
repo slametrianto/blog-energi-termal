2852e1d3a8814cb8b00139df380c81ba4d03731e
slamet rianto <slametrianto25@gmail.com>
2020-04-24 11:38:42 +0700

    blog

---
+++ application/views/blog/acpresisiliebert.php
@@ -0,0 +1,119 @@
+
+<div class="jumbotron" style="background-image: url('assets/img/')">
+    <div class="container">
+    </div>
+</div>
+
+
+<!-- Post Content -->
+<article>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6 col-md-10 mx-auto">
+                <h2 class="section-heading">Ac Presisi Liebert</h2>
+                <p align="justify">adalah sistem Instalasi sistem pendingin pada desain data center sebuah AC (Air Conditioning) presisi. Tipe AC presisi dapat mendinginkan seluruh ruang server, bahkan hingga seluruh rak server yang telah terisi oleh server dan perangkat jaringan lainnya. Namun tidak menutup kemungkinan juga memasang sistem pendingin sehingga apabila satu perangkat mengalami kerusakan, akan ada perangkat cadangan yang mengambil alih fungsinya</p>
+
+                <p align="justify">Sebagai Vertiv, mereka terus menjadi penyedia terkemuka global teknologi infrastruktur kritis dan layanan siklus hidup, dan berkomitmen seperti sebelumnya untuk memberdayakan aplikasi vital dunia digital, mendukung pasar mobile, cloud, dan komputasi tepi yang berkembang saat ini. Dengan merek-merek unggulan, seperti ASCO®, Chloride®, Liebert®, NetSure ™, dan Trellis ™, mereka memiliki portofolio yang luas dari solusi & layanan manajemen daya, termal dan infrastruktur untuk mendukung aplikasi penting di pusat data, jaringan komunikasi, dan komersial dan lingkungan industri.</p>
+
+                <a href="#">
+                    <img class="img-fluid" src="assets/img/acconditioning.jpg" alt="">
+                </a>
+                <span class="caption text-muted">Information Technology:</span>
+
+                </br>
+                </br>
+                </br>
+
+                <p align="justify">Data Centre Air Conditioning Design</p>
+
+                <p align="justify">Computer Room Air Conditioning, efficiency cooling  , mengontrol kelembaban udara ruagan dan pendistribusian udara.</p>
+
+                <p align="justify">esting and Commissioning Precision Air Conditioning</p>
+
+                <h2 class="section-heading">Design Engineering</h2>
+
+                <p align="justify">Design Refrigeration and Air Conditioning system</p>
+
+                <p align="justify">Cooling system for Industrial refrigeration and air conditioning</p>
+
+                <!-- <blockquote class="blockquote">merupakan jenis UPS yang dipakai untuk para pemilik bisnis kecil yang sudah membutuhkan jumlah arus listrik yang lebih besar. Kemampuannya di atas UPS standby dengan harga yang juga tentunya lebih mahal. Beberapa server milik pemerintah juga banyak yang menggunakan UPS jenis ini. Tingkat reliabilitasnya lebih tinggi dan mampu menyesuaikan jumlah voltase yang lebih tinggi dari peralatan.</blockquote> -->
+
+                <p align="justify">HVAC for Industrial Processing</p>
+
+                <h2 class="section-heading">Preventive Maintenance Precision Air Conditioning</h2>
+
+                <p align="justify">Service and supply spare part.</p>
+
+                <a href="#">
+                    <img class="img-fluid" src="assets/img/acpresisi.jpg" alt="">
+                </a>
+                </br>
+                </br>
+                <span class="caption text-muted">Data Centre Power.</span>
+
+                <h2 class="section-heading">Air Conditioning / Ac Presisi</h2>
+
+                <p align="justify">Preventive Maintenance Precision Air Conditioning, Service and supply spare part and Data Centre Power.</p>
+
+            </div>
+
+            <div class="row">
+                <div class="col-lg-6">
+                    <div class="card" style="width: 18rem;">
+                        <div class="card-header">
+                            Artikel
+                        </div>
+                        <div class="card" style="width: 18rem;">
+                            <ul class="list-group ">
+                                <a class="list-group-item" href="pengertianups">Pengertian UPS, Jenis UPS,....</a>
+                                <a class="list-group-item" href="vertiv">Ruang Server Mini Dalam Satu,....</a>
+                                <a class="list-group-item" href="vertivsmartcabinet">Vertiv (formerly Emerson Network Power)</a>
+                                <a class="list-group-item" href="acpresisiliebert">Ac Presisi Liebert</a>
+                                <a class="list-group-item" href="artikelempat">Blog sample</a>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</article>
+
+<hr>
+
+<!-- Footer -->
+<footer>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-8 col-md-10 mx-auto">
+                <ul class="list-inline text-center">
+                    <li class="list-inline-item">
+                        <a href="#">
+                            <span class="fa-stack fa-lg">
+                                <i class="fas fa-circle fa-stack-2x"></i>
+                                <i class="fab fa-twitter fa-stack-1x fa-inverse"></i>
+                            </span>
+                        </a>
+                    </li>
+                    <li class="list-inline-item">
+                        <a href="#">
+                            <span class="fa-stack fa-lg">
+                                <i class="fas fa-circle fa-stack-2x"></i>
+                                <i class="fab fa-facebook-f fa-stack-1x fa-inverse"></i>
+                            </span>
+                        </a>
+                    </li>
+                    <li class="list-inline-item">
+                        <a href="#">
+                            <span class="fa-stack fa-lg">
+                                <i class="fas fa-circle fa-stack-2x"></i>
+                                <i class="fab fa-github fa-stack-1x fa-inverse"></i>
+                            </span>
+                        </a>
+                    </li>
+                </ul>
+                <p class="copyright text-muted">Blog &copy; Energi Termal Solusi Your Website 2019</p>
+            </div>
+        </div>
+    </div>
+</footer>
